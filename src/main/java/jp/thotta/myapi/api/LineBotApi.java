fb861dead4467858cba7173ff1d78bf452409223
Toru Hotta <toru1055h@gmail.com>
2017-08-12 11:40:22 +0900

    add line bot

---
+++ src/main/java/jp/thotta/myapi/api/LineBotApi.java
@@ -0,0 +1,25 @@
+package jp.thotta.myapi.api;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+/**
+ * Created by thotta on 2017/08/12.
+ */
+@LineMessageHandler
+public class LineBotApi {
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}
