8b15aad5423da13ffabb1b441f6f7ea66f378e3d
Toru Hotta <toru1055h@gmail.com>
2017-08-12 14:46:57 +0900

    add location message handler

--- src/main/java/jp/thotta/myapi/api/LineBotApi.java
+++ src/main/java/jp/thotta/myapi/api/LineBotApi.java
@@ -2,7 +2,9 @@ package jp.thotta.myapi.api;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.LocationMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.LocationMessage;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
@@ -16,6 +18,14 @@ public class LineBotApi {
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
         return new TextMessage(event.getMessage().getText() + " でんがな");
+    }
+
+    @EventMapping
+    public TextMessage handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
+        System.out.println("event: " + event);
+        String lat = String.format("緯度: %.3f", event.getMessage().getLatitude());
+        String lon = String.format("経度: %.3f", event.getMessage().getLongitude());
+        return new TextMessage(lat + "\n" + lon);
     }
 
     @EventMapping
